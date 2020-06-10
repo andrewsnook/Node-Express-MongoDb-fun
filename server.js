cbc6c23ae08515347822c719c800dabf6959d192
Andrew <admin@losthobbies.com>
2020-06-11 00:05:07 +0100

    added mongoose and colors etc

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
+const morgan = require('morgan');
+const colours = require('colors');
+const connectDB = require('./config/db');
+
+// environment vars
+dotenv.config({path: './config/config.env'});
+
+// connect to db
+connectDB();
+
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
-dotenv.config({path: './config/config.ev'});
-
 const app = express();
 
-
-
-app.use(logger);
+// dev logging middleware
+if(process.env.NODE_ENV === "development"){
+    app.use(morgan('dev'));
+}
 
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT, 
-    console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}` .yellow.bold)
 );
+
+//handle 
+process.on('unhandledRejection', (err,promise) => {
+    console.log(`Error:${err.message}`);
+    // close server and exit
+    server.close(() => process.exit(1));
+});
