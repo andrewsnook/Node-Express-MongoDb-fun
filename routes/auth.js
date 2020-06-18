a8df7d1ded6a25ca64b0563db66a3c482cd54107
Andrew <admin@losthobbies.com>
2020-06-18 23:41:17 +0100

    lecture 58

--- routes/auth.js
+++ routes/auth.js
@@ -4,15 +4,17 @@ const {
   login,
   getMe,
   forgotPassword,
+  resetPassword,
 } = require("../controllers/auth");
 
 const router = express.Router();
 
 const { protect } = require("../middleware/auth");
+const { reset } = require("nodemon");
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/me", protect, getMe);
 router.post("/forgotpassword", forgotPassword);
-
+router.put("/resetpassword/:resettoken", resetPassword);
 module.exports = router;
